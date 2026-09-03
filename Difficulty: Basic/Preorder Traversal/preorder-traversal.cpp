5905424b5c6b68752972191bf8e3cd1653b4feb0
Sachin Kumar <166536341+sachinkumar9090@users.noreply.github.com>
2026-09-03 23:42:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Preorder Traversal/preorder-traversal.cpp
@@ -0,0 +1,32 @@
+/* Structure of Tree Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};*/
+
+void preorder(Node *root,vector<int>&ans){
+    if(root==NULL){
+        return;
+    }
+    ans.push_back(root->data);
+    preorder(root->left,ans);
+    preorder(root->right,ans);
+}
+
+class Solution {
+  public:
+    vector<int> preOrder(Node* root) {
+        // code here
+        vector<int>ans;
+        preorder(root,ans);
+        return ans;
+    }
+};
