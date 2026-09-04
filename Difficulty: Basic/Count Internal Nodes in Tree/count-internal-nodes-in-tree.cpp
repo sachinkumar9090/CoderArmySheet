4c6a775ea128992e65bb49457b7182214bfff641
Sachin Kumar <166536341+sachinkumar9090@users.noreply.github.com>
2026-09-04 18:28:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Internal Nodes in Tree/count-internal-nodes-in-tree.cpp
@@ -0,0 +1,30 @@
+/* Binary Tree Node Structure
+class Node {
+    public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int countNonLeafNodes(Node* root) {
+        // Code here
+        if(root==NULL){
+            return 0;
+        }
+        
+        if(!root->left && !root->right){
+            return 0;
+        }
+        
+        return (1+ countNonLeafNodes(root->left) + countNonLeafNodes(root->right));
+        
+    }
+};
