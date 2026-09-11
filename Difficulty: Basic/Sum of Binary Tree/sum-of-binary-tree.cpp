ac77ae01736d27303be0c10b7df21238dbd96feb
Sachin Kumar <166536341+sachinkumar9090@users.noreply.github.com>
2026-09-11 14:31:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sum of Binary Tree/sum-of-binary-tree.cpp
+++ Difficulty: Basic/Sum of Binary Tree/sum-of-binary-tree.cpp
@@ -10,22 +10,14 @@ public:
     }
 };*/
 
-void getsum(Node *root,int &sum){
-    if(root==NULL){
-        return;
-    }
-    sum +=root->data;
-    getsum(root->left,sum);
-    getsum(root->right,sum);
-}
-
 class Solution {
   public:
     int sumBT(Node* root) {
         // code here
-        int sum =0;
-        getsum(root,sum);
-        return sum;
+        if(!root){
+            return 0;
+        }
         
+        return root->data + sumBT(root->left) + sumBT(root->right);
     }
 };
