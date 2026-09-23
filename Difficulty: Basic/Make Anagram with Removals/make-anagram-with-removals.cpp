9871317efd320cf71eb394dd382cefe277459aab
Sachin Kumar <166536341+sachinkumar9090@users.noreply.github.com>
2026-09-23 18:18:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Make Anagram with Removals/make-anagram-with-removals.cpp
+++ Difficulty: Basic/Make Anagram with Removals/make-anagram-with-removals.cpp
@@ -2,16 +2,19 @@ class Solution {
   public:
     int remAnagram(string &s1, string &s2) {
         // code here
-        vector<int> alpha(26,0);
-        int count = 0;
+        vector<int> lower(26,0);
         for(int i=0;i<s1.size();i++){
-            alpha[s1[i]-'a']++;
+            lower[s1[i]-'a']++;
         }
+        
         for(int i=0;i<s2.size();i++){
-            alpha[s2[i]-'a']--;
+            lower[s2[i]-'a']--;
         }
+        
+        int count = 0;
         for(int i=0;i<26;i++){
-            count +=abs(alpha[i]);
+            count+=abs(lower[i]);
+    
         }
         return count;
     }
